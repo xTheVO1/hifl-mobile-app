999c47f4ef1e14c9bd1c071973491227931339bf
Victor Olaitan <vividvictor@gmail.com>
2022-06-05 22:53:08 +0100

    Implemented New Fan and Home List

--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -10,8 +10,118 @@ import {
   TouchableOpacity,
   StatusBar,
   Pressable,
+  FlatList,
 } from "react-native";
 import tw from "../lib/tailwind";
+const DATA = [
+  {
+    id: "908",
+    firstName: "Victor",
+    lastName: "Olaitan",
+    phoneNumber: "0908098765",
+    email: "vivo@gmail.com",
+  },
+  {
+    id: "108",
+    firstName: "John",
+    lastName: "Doe",
+    phoneNumber: "0809876543",
+    email: "jdoe@gmail.com",
+  },
+  {
+    id: "0098",
+    firstName: "Jane",
+    lastName: "Doe",
+    phoneNumber: "08156533938",
+    email: "janedoe@gmail.com",
+  },
+  {
+    id: "978",
+    firstName: "Victor",
+    lastName: "Olaitan",
+    phoneNumber: "0908098765",
+    email: "vivo@gmail.com",
+  },
+  {
+    id: "208",
+    firstName: "John",
+    lastName: "Doe",
+    phoneNumber: "0809876543",
+    email: "jdoe@gmail.com",
+  },
+  {
+    id: "098",
+    firstName: "Jane",
+    lastName: "Doe",
+    phoneNumber: "08156533938",
+    email: "janedoe@gmail.com",
+  },
+  {
+    id: "9008",
+    firstName: "Victor",
+    lastName: "Olaitan",
+    phoneNumber: "0908098765",
+    email: "vivo@gmail.com",
+  },
+  {
+    id: "1083",
+    firstName: "John",
+    lastName: "Doe",
+    phoneNumber: "0809876543",
+    email: "jdoe@gmail.com",
+  },
+  {
+    id: "134",
+    firstName: "Jane",
+    lastName: "Doe",
+    phoneNumber: "08156533938",
+    email: "janedoe@gmail.com",
+  },
+  {
+    id: "9748",
+    firstName: "Victor",
+    lastName: "Olaitan",
+    phoneNumber: "0908098765",
+    email: "vivo@gmail.com",
+  },
+  {
+    id: "2058",
+    firstName: "John",
+    lastName: "Doe",
+    phoneNumber: "0809876543",
+    email: "jdoe@gmail.com",
+  },
+  {
+    id: "04968",
+    firstName: "Jane",
+    lastName: "Doe",
+    phoneNumber: "08156533938",
+    email: "janedoe@gmail.com",
+  },
+];
+
+const Item = ({ firstName, lastName }) => (
+  <View style={tw`w-full p-5 my-1 flex flex-col content-center bg-[#FAFAFA]`}>
+    <View style={tw`flex flex-row justify-between`}>
+      <View style={tw`flex flex-row `}>
+        <Image
+          source={require("../assets/icons/girl.png")}
+          style={tw`w-[48px] h-[48px] mr-5`}
+        />
+        <View style={tw`flex flex-col content-center`}>
+          <Text style={tw`text-[#000229] text-lg font-light	`}>
+            {firstName} {lastName}
+          </Text>
+          <Text style={tw`text-xs text-[#0013FF] font-light	`}>10 TICKETS</Text>
+        </View>
+      </View>
+      <View style={tw`flex flex-col `}>
+        <Text style={tw`text-xs font-light`}>Last Updated</Text>
+        <Text style={tw`text-xs	font-extralight	`}>June 01, 2022</Text>
+      </View>
+    </View>
+  </View>
+);
 
 const Home = ({ navigation }) => {
   const initialState = { email: "", password: "" };
@@ -19,28 +129,50 @@ const Home = ({ navigation }) => {
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = () => {
-    console.log("Clicked");
-  };
-
-  const createNewFan = () => {
     navigation.navigate("newFan");
   };
 
+  const renderItem = ({ item }) => (
+    <Item
+      firstName={item.firstName}
+      lastName={item.lastName}
+      phoneNumber={item.phoneNumber}
+    />
+  );
   return (
     <SafeAreaView style={tw`bg-secondary h-full relative`}>
       <ScrollView style={tw`flex flex-col bg-secondary`}>
         <View style={tw`h-[200px] flex flex-col justify-end pb-5`}>
           <View style={tw`flex flex-row justify-between items-center px-6`}>
-            <Text style={tw`text-[#fff] font-bold text-2xl`}>Hi Feji</Text>
-            <Image source={require("../assets/hifl_icon.png")} style={tw`w-[50px]`} />
+            <Text style={tw`text-[#fff] font-bold text-2xl`}>Hi Feji,</Text>
+            <Image
+              source={require("../assets/hifl_icon.png")}
+              style={tw`w-[50px]`}
+            />
           </View>
         </View>
 
-        <View style={tw`h-[498px] bg-[#fff] rounded-t-[25px] py-15 px-6`}>
-          <View style={tw``}></View>
+        <View style={tw`h-[100%] bg-[#fff] rounded-t-[25px] py-15 `}>
+          <View style={tw`px-6`}>
+            <Text>Search Area</Text>
+          </View>
+          <View style={tw`my-8 w-full`}>
+            <FlatList
+              data={DATA}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id}
+            />
+          </View>
         </View>
-        <TouchableOpacity activeOpacity={0.7} onPress={createNewFan} style={tw`absolute bottom-8 right-6`}>
-          <Image source={require("../assets/icons/add.png")} style={tw`w-[60px] h-[60px]`} />
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={handleSubmit}
+          style={tw`absolute bottom-8 right-6`}
+        >
+          <Image
+            source={require("../assets/icons/add.png")}
+            style={tw`w-[60px] h-[60px]`}
+          />
         </TouchableOpacity>
       </ScrollView>
     </SafeAreaView>
--- app/screens/NewFan.jsx
+++ app/screens/NewFan.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   ScrollView,
   View,
@@ -10,23 +10,127 @@ import {
   TouchableOpacity,
   StatusBar,
   Pressable,
+  SectionList,
 } from "react-native";
 import tw from "../lib/tailwind";
 
-const NewFan = () => {
+const Gender = [
+  {
+    title: "Gender",
+    data: ["Male", "Female"],
+  },
+];
+
+const Item = ({ title }) => (
+  <View style={tw``}>
+    <Text style={tw``}>{title}</Text>
+  </View>
+);
+
+const NewFan = ({ navigation }) => {
+  const handleSubmit = () => {
+    console.log("Clicked");
+  };
+  const handleHome = () => {
+    navigation.navigate("home");
+  };
+  const handleLogout = () => {
+    navigation.navigate("login");
+  };
+  const initialState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    createdBy: "",
+  };
+  const [fan, setFan] = useState(initialState);
+  const [loading, setLoading] = useState(false);
   return (
     <SafeAreaView style={tw`bg-secondary h-full relative`}>
       <ScrollView style={tw`flex flex-col bg-secondary`}>
         <View style={tw`h-[200px] flex flex-col justify-end pb-5`}>
           <View style={tw`flex flex-row justify-between items-center px-6`}>
-            <Image source={require("../assets/icons/home.png")} style={tw``} />
+            <Pressable onPress={handleHome}>
+              <Image
+                source={require("../assets/icons/home.png")}
+                style={tw``}
+              />
+            </Pressable>
             <Text style={tw`text-[#fff] font-bold text-2xl`}>New Fan</Text>
-            <Image source={require("../assets/hifl_icon.png")} style={tw`w-[50px]`} />
+
+            <Pressable onPress={handleLogout}>
+              <Image
+                source={require("../assets/hifl_icon.png")}
+                style={tw`w-[50px]`}
+              />
+            </Pressable>
           </View>
         </View>
 
-        <View style={tw`h-[498px] bg-[#fff] rounded-t-[25px] py-15 px-6`}>
-          <View style={tw``}></View>
+        <View style={tw` bg-[#fff] rounded-t-[25px] py-5 px-6`}>
+          <View style={tw`my-6`}>
+            <TextInput
+              style={tw`py-3 text-sm border w-full border-[#E5E5E5] focus:border-primary rounded px-4 mb-6 focus:outline-none`}
+              placeholder="First Name"
+              name="firstName"
+              onChangeText={(value) => setFan({ ...fan, firstName: value })}
+              value={fan.firstName}
+              autoComplete="off"
+            />
+            <TextInput
+              style={tw`py-3 text-sm border w-full border-[#E5E5E5] focus:border-primary rounded px-4 mb-6 focus:outline-none`}
+              placeholder="Last Name"
+              name="lastName"
+              onChangeText={(value) => setFan({ ...fan, lastName: value })}
+              value={fan.lastName}
+              autoComplete="off"
+            />
+            <TextInput
+              style={tw`py-3 text-sm border w-full border-[#E5E5E5] focus:border-primary rounded px-4 mb-6 focus:outline-none`}
+              placeholder="Phone Number"
+              name="phoneNumber"
+              onChangeText={(value) => setFan({ ...fan, phoneNumber: value })}
+              value={fan.phoneNumber}
+              keyboardType="number-pad"
+              maxLength={11}
+              autoComplete="off"
+            />
+            <TextInput
+              style={tw`py-3 text-sm border w-full border-[#E5E5E5] focus:border-primary rounded px-4 mb-6 focus:outline-none`}
+              placeholder="Email Address"
+              name="email"
+              onChangeText={(value) => setFan({ ...fan, email: value })}
+              value={fan.email}
+              keyboardType="email-address"
+              autoComplete="off"
+            />
+
+            <TextInput
+              style={tw`py-3 text-sm border w-full border-[#E5E5E5] focus:border-primary rounded px-4 mb-10 focus:outline-none`}
+              placeholder="Ticket Number"
+              name="ticket"
+              onChangeText={(value) => setFan({ ...fab, ticket: value })}
+              value={fan.ticket}
+              secureTextEntry
+              keyboardType="number-pad"
+              autoComplete="off"
+            />
+            <TouchableOpacity
+              style={tw`py-4 w-full bg-primary flex flex-row justify-center rounded-md`}
+              activeOpacity={0.7}
+              // disabled={}
+              onPress={handleSubmit}
+            >
+              {loading ? (
+                <ActivityIndicator size="small" color="#fff" />
+              ) : (
+                <Text style={tw`text-[#fff] text-sm font-bold capitalize`}>
+                  Submit
+                </Text>
+              )}
+            </TouchableOpacity>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
